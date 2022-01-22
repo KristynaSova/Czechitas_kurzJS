ea1bb55c9844768e9a019494408fbd6de1ef1d6c
KristynaSova <ksovova1@gmail.com>
2022-01-22 14:49:51 +0100

    1 file modified

--- ukol4.js
+++ ukol4.js
@@ -1,2 +1,9 @@
-let i = Number(prompt('Napis cislo'));
-console.log(i);
+let vstup = Number(prompt('Zadej mi cislo co neni nula'));
+let soucet = 0;
+while (vstup !== 0) {
+  vstup = Number(prompt('Zadej mi cislo co neni nula'));
+  soucet = soucet + vstup;
+  console.log('Celkovy soucet:' + soucet);
+}
+
+console.log('Celkovy soucet:' + soucet);
