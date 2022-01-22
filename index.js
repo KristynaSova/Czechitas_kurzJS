13f5e59d76dd9ce0b6770cd58f87bdde9a28e07f
KristynaSova <ksovova1@gmail.com>
2022-01-22 11:24:06 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import './ukol1.js';
 import './ukol2.js';
 
+
+
 let jmeno = 'Kristyna';
 let vek = 37;
 let vaha = 60;
 let vyska = 1.77;
-let a = 60/ 1.77**2
-console.log ('a');
+let bmi = vaha / vyska ** 2;
+console.log(bmi);
--- ukol1.js
+++ ukol1.js
@@ -1,4 +1,9 @@
 console.log('ahoj');
 
-
-
+let jmeno = 'Kristyna';
+let vek = 37;
+let vaha = 60;
+let vyska = 1.77;
+let bmi = vaha / vyska ** 2;
+console.log(bmi);
+console.log('jmenuji se ' + jmeno + 'je mi' + vek + 'muj bmi je' + bmi);
