58c25a3ef217fd2988237678852c0cb51acdfaa1
KristynaSova <ksovova1@gmail.com>
2022-01-22 12:19:35 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import './ukol1.js';
 import './ukol2.js';
 
-let jmeno = 'Kristyna';
+/* let jmeno = 'Kristyna';
 let vek = 37;
 let vaha = 60;
 let vyska = 1.77;
 let bmi = vaha / vyska ** 2;
 console.log(bmi);
 
+
 let docasna = prompt('Ahoj, kolik je ti let?');
 let odpoved = docasna;
 console.log(odpoved);
 // Number pretypuje na cislo
 
+
+let jeZima = false;
+let svitiSlunicko = true;
+// jeZima A svitiSlunicko
+// || znamena alespon jedna je pravad
+
+console.log(5 === 5);
+
+let vek =34;
+if (vel < 18) {
+  console.log('Nejsi plnolety');
+} esle {
+
+  console.log('jsi plnolety')
+}
+
+*/
+
+let odpoved = prompt('Ahoj, jak se jmenuejes?');
+console.log(odpoved);
